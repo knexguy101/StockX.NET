e4edd3f79fa593c56fbf137f3dc633c76dfef08b
Chris Falb <chris.falb@hotmail.com>
2019-09-30 23:23:36 -0500

    Added Login

---
+++ StockX.NET/Models/PortfolioItem.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockX.NET.Models
+{
+    public class PortfolioItem
+    {
+        public string ChainID { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int ListingAmount { get; set; }
+        public int LocalMarketValue { get; set; }
+        public Product Product { get; set; }
+        public string ProductID { get; set; }
+        public string SKUUID { get; set; }
+
+        public void LoadItem(JObject Object)
+        {
+
+        }
+    }
+}
